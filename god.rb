797e0e20da415f6b15f01e76a40de03d943a48e8
Rixele <liamrn94@gmail.com>
2019-02-28 17:30:17 -0500

    add god

---
+++ god.rb
@@ -0,0 +1,18 @@
+class Man
+
+    def initialize(creation1,creation2)
+
+        attr_accessor :creation
+
+        ary = ["man","women"]
+
+        @ary = ["Adam","Eva"]
+
+    end
+
+end
+
+man = Man.new("as","asas")
+
+puts "#{man.creation}"
+
